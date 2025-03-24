6bddbfcd35f13002f3412800fe5aad00476e8ac3
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-03-25 05:39:32 +0700

    prosedur input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,32 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukan maksimal 20 Elemen.\n";
+        }
+    }  
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++) 
+    {
+        cout << "Data ke-" << (i + 1) << ": "; 
+        cin >> arr[i];
+    } 
+}
+   
